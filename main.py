2ef78455b18807d50b50e1a7bf8243c5e65e106c
Kalp Shah <55651413+SpideyOnHigh@users.noreply.github.com>
2023-01-27 20:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import pandas as pd
 app = Flask(__name__)
 
 enc = pickle.load(open('enc.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
-
 
 @app.route('/recommend',methods = ['POST'])
 def recommend():
@@ -18,13 +16,11 @@ def recommend():
     prep_time = request.form.get('prep_time')
     budget = request.form.get('budget')
     type = request.form.get('type')
-    
-    data = data = [[veg_or_nonveg, taste, prep_time, budget, type]]
 
-    input_features = pd.DataFrame(data, columns=['veg_or_nonveg', 'Taste', 'Prep Time', 'Budget', 'Type'])
+    input_features = pd.DataFrame(
+        {'veg_or_nonveg': [veg_or_nonveg], 'Taste': [taste], 'Prep Time': [prep_time], 'Budget': [budget],
+         'Type': [type]})
 
-    print(input_features)
-   
 
     # One-hot encode the input features
     input_features = enc.transform(input_features).toarray()
@@ -34,4 +30,5 @@ def recommend():
 
 
 if __name__ == '__main__':
+    model = pickle.load(open('model.pkl','rb'))
     app.run(debug=True)
